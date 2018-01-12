174d2f925df01bac6f894eaa9439225863ed908f
Brendan Gregg <bgregg@netflix.com>
2018-01-12 15:27:59 -0800

    add more options

--- wss.pl
+++ wss.pl
@@ -2,7 +2,10 @@
 #
 # wss	Estimate the working set size (WSS) for a process on Linux.
 #
-# USAGE: wss PID duration(s)
+# USAGE: wss [options] PID duration(s)
+#    eg,
+#        wss 181 0.01	# measure PID 181 WSS for 10 milliseconds
+#        wss -h		# full USAGE
 #
 # COLUMNS:
 #	- RSS(MB): Resident Set Size (Mbytes). The main memory size.
@@ -30,24 +33,38 @@
 
 use strict;
 use Getopt::Long;
+$| = 1;
 
 sub usage {
 	die <<USAGE_END;
 USAGE: wss [options] PID duration(s)
-	-i secs            # repeat output every secs seconds
+	-C         # show cumulative output every duration(s)
+	-s secs    # show a duration(s) snapshot every secs
+	-d secs    # total duration of measuremnt (for -s or -C)
    eg,
-	wss 181 0.01       # measure PID 181 for 10 milliseconds
-	wss -i 1 181 0.01  # ... and print output every second
+	wss 181 0.01       # measure PID 181 WSS for 10 milliseconds
+	wss 181 5          # measure PID 181 WSS for 5 seconds (same overhead)
+	wss -C 181 5       # show PID 181 growth every 5 seconds
+	wss -Cd 10 181 1   # PID 181 growth each second for 10 seconds total
+	wss -s 1 181 0.01  # show a 10 ms WSS snapshot every 1 second
 USAGE_END
 }
 
-my $interval = 0;
+my $snapshot = 0;
+my $totalsecs = 999999999;
+my $cumulative = 0;
 GetOptions(
-	'interval|i=f'  => \$interval,
+	'snapshot|s=f'  => \$snapshot,
+	'duration|d=f'  => \$totalsecs,
+	'cumulative|C'  => \$cumulative,
 ) or usage();
 
 if (@ARGV < 2 || $ARGV[0] eq "-h" || $ARGV[0] eq "--help") {
 	usage();
+	exit;
+}
+if ($cumulative and $snapshot) {
+	print STDERR "ERROR: Can't use -C and -s (doesn't make much sense). Exiting.\n";
 	exit;
 }
 my $pid = $ARGV[0];
@@ -56,14 +73,30 @@ my $clear_ref = "/proc/$pid/clear_refs";
 my $smaps = "/proc/$pid/smaps";
 my ($rss, $pss, $referenced);
 my $metric;
+my $reset = 0;
+my $time = 0;
 
+# headers
+if ($cumulative) {
+	printf "Watching PID $pid page references grow, output every $duration seconds...\n";
+} elsif ($snapshot) {
+	printf "Watching PID $pid page references for $duration seconds, once every $snapshot seconds...\n";
+} else {
+	printf "Watching PID $pid page references during $duration seconds...\n";
+}
 printf "%10s %10s %10s\n", "RSS(MB)", "PSS(MB)", "Ref(MB)";
+
+# main
 while (1) {
-	open CLEAR, ">$clear_ref" or die "ERROR: can't open $clear_ref (older kernel?): $!";
-	print CLEAR "1";
-	close CLEAR;
+	if (not $cumulative or not $reset) {
+		open CLEAR, ">$clear_ref" or die "ERROR: can't open $clear_ref (older kernel?): $!";
+		print CLEAR "1";
+		close CLEAR;
+		$reset = 1;
+	}
 
 	select(undef, undef, undef, $duration);
+	$time += $duration;
 
 	$rss = $pss = $referenced = 0;
 	open SMAPS, $smaps or die "ERROR: can't open $smaps: $!";
@@ -85,9 +118,14 @@ while (1) {
 
 	printf "%10.2f %10.2f %10.2f\n", $rss / 1024, $pss / 1024, $referenced / 1024;
 
-	if ($interval) {
-		sleep($interval);
-	} else {
+	if ($snapshot) {
+		sleep($snapshot);
+		$time += $snapshot;
+	} elsif (not $cumulative) {
+		last;
+	}
+
+	if ($time >= $totalsecs) {
 		last;
 	}
 }
