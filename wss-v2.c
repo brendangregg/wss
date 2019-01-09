1f1d19cae02ebc574cfd4c46a0b3cb5f5ef8ba88
DongDongJu <commisori28@gmail.com>
2019-01-09 12:38:13 +0900

    ADD: print new line when get result in v2

--- wss-v2.c
+++ wss-v2.c
@@ -313,7 +313,7 @@ int main(int argc, char *argv[])
 	// assume getpagesize() sized pages:
 	mbytes = (g_activepages * getpagesize()) / (1024 * 1024);
 	printf("%-7s %10s\n", "Est(s)", "Ref(MB)");
-	printf("%-7.3f %10.2f", (double)est_us / 1000000, mbytes);
+	printf("%-7.3f %10.2f\n", (double)est_us / 1000000, mbytes);
 
 	return 0;
 }
