58bb7809e3056838521e5b1aee6a6fffa9d3dd9b
James Gowans <jgowans@amazon.com>
2019-06-12 16:34:43 +0200

    create_gif does multi colour and graph
    Sorry for the "and."

--- create_gif.rb
+++ create_gif.rb
@@ -1,65 +1,92 @@
 #!/usr/bin/env ruby
 
+require 'time'
 require 'rmagick'
+require 'gruff'
 
-pid = "10092"
-virtual_address = "0x7fd607823000"
+if ARGV.length != 1
+  puts("Usage: create_gif.rb pid")
+  exit
+end
+pid = ARGV[0]
 
 Dir.chdir("/tmp/wss/#{pid}")
-timestamps = Dir.glob("155*").sort
+timestamps = Dir.glob("20*").sort
 puts("Using timestamps #{timestamps}")
+virtual_addresses = Dir.glob("#{timestamps[-2]}/0x*").map{|f| f.split("/").last}
+if virtual_addresses.length != 1
+  puts("Got invalid number of virtual addresses: #{virtual_addresses}")
+end
+virtual_address = virtual_addresses.first
 
-image_size = Math.sqrt(File.size("#{timestamps[-2]}/#{virtual_address}") * 4).ceil()
+# This will get messy if we add in page content hashes...
+image_size = Math.sqrt(File.size("#{timestamps[-2]}/#{virtual_address}")).ceil()
 puts("Using image size #{image_size} from file size #{File.size("#{timestamps[-2]}/#{virtual_address}")}")
+
+label_border = (image_size / 30.0).ceil()
 
 gif = Magick::ImageList.new
 
+active_pages_arr = Array.new
+zero_pages_arr = Array.new
+mapped_pages_arr = Array.new
+
 pixels = Array.new(3 * image_size * image_size, 0)
-masks = (0...4).map { |shift| 0x3 << (2 * shift) }
-puts(masks)
 (0...timestamps.size).each do |timestamp_idx|
   puts("#{timestamp_idx} / #{timestamps.size}")
   file = File.open("#{timestamps[timestamp_idx]}/#{virtual_address}")
-
-  byte_cnt = 0
+  active_pages = 0
+  zero_pages = 0
+  mapped_pages = 0
+  page_idx = 0
   file.each_byte do |byte|
-    masks.each_with_index do |mask, mask_idx|
-      page_idx = (byte_cnt * 4) + mask_idx
-      if ((byte & mask) == 0)
-        pixels[(3 * page_idx) + 0 ] = 0
-        pixels[(3 * page_idx) + 1 ] = 0
-        pixels[(3 * page_idx) + 2 ] = 0
-      elsif ((byte & mask) == (0xAA & mask))
-        pixels[(3 * page_idx) + 0 ] = 0
-        pixels[(3 * page_idx) + 1 ] = 0x7FFF
-        pixels[(3 * page_idx) + 2 ] = 0
-      elsif ((byte & mask) == (0xFF & mask))
-        pixels[(3 * page_idx) + 0 ] = 0x7FFF
-        pixels[(3 * page_idx) + 1 ] = 0
-        pixels[(3 * page_idx) + 2 ] = 0
-      else
-        puts("Unknown byte: #{byte & mask}")
-        exit
-      end
+    activity_mask = 0x03
+    zero_mask = 1 << 2
+    zero_add = ((byte & zero_mask) == 0) ? 0 : 0x8000
+    zero_pages += 1 if zero_add != 0
+    if ((byte & activity_mask) == 0)
+      pixels[(3 * page_idx) + 0 ] = 0
+      pixels[(3 * page_idx) + 1 ] = 0
+      pixels[(3 * page_idx) + 2 ] = 0
+    elsif ((byte & activity_mask) == 0x02)
+      pixels[(3 * page_idx) + 0 ] = 0
+      pixels[(3 * page_idx) + 1 ] = 0x7FFF + zero_add
+      pixels[(3 * page_idx) + 2 ] = 0
+      mapped_pages += 1
+    elsif ((byte & activity_mask) == 0x03)
+      pixels[(3 * page_idx) + 0 ] = 0x7FFF + zero_add
+      pixels[(3 * page_idx) + 1 ] = 0
+      pixels[(3 * page_idx) + 2 ] = 0
+      active_pages += 1
+      mapped_pages += 1
+    else
+      puts("Unknown byte: #{byte & mask}")
+      exit
     end
-    byte_cnt += 1
+    page_idx += 1
   end
-  image = Magick::Image.new(image_size, image_size) { self.background_color = "red" }
+
+  active_pages_arr << active_pages / page_idx.to_f
+  zero_pages_arr << zero_pages / page_idx.to_f
+  mapped_pages_arr << mapped_pages / page_idx.to_f
+
+  image = Magick::Image.new(image_size, image_size) { self.background_color = "black" }
   puts("Image size: #{image_size}")
   puts("Pixel size: #{pixels.length}")
   image.import_pixels(0, 0, image_size, image_size, "RGB", pixels)
+  image = image.extent(image_size, image_size + label_border)
   desc_txt = Magick::Draw.new
-  image.annotate(desc_txt, 0, 0, 0, 0, "SPECjbb 2 core, 16 GiB") {
-    desc_txt.pointsize = 50
+  image.annotate(desc_txt, 0, 0, 0, 0, "SPECjbb 1 core, 3100 MiB RAM, 2500 MiB heap") {
+    desc_txt.pointsize = label_border * 0.9
     desc_txt.fill = 'red'
     desc_txt.gravity = Magick::SouthWestGravity
     desc_txt.font_weight = Magick::BoldWeight
   }
   frame_txt = Magick::Draw.new
-  delta_t = timestamps[timestamp_idx].to_i - timestamps.first.to_i
+  delta_t = Time.parse(timestamps[timestamp_idx]) - Time.parse(timestamps.first)
   image.annotate(frame_txt, 0, 0, 0, 0, Time.at(delta_t).utc.strftime("%H:%M:%S")) {
-    frame_txt.pointsize = 50
-    frame_txt.fill = 'red'
+    frame_txt.pointsize = label_border * 0.9
+    frame_txt.fill = 'white'
     frame_txt.gravity = Magick::SouthEastGravity
     frame_txt.font_weight = Magick::BoldWeight
   }
@@ -69,3 +96,9 @@ puts(masks)
 end
 gif.delay = 100
 gif.write("img/gif.gif")
+
+graph = Gruff::Line.new
+graph.data("mapped", mapped_pages_arr)
+graph.data("zeros", zero_pages_arr)
+graph.data("active", active_pages_arr)
+graph.write("img/graph.png")
